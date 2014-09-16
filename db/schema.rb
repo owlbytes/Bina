3132724d333381912263eae05ffc9a5db51d31a4
owlbytes <neshah07@gmail.com>
2014-09-16 22:47:55 +0100

    created signup and login features

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140913224102) do
+ActiveRecord::Schema.define(:version => 20140916202318) do
 
   create_table "abouts", :force => true do |t|
     t.string   "subtitle"
@@ -68,6 +68,13 @@ ActiveRecord::Schema.define(:version => 20140913224102) do
     t.text     "description"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
   end
 
   create_table "vessels", :force => true do |t|
