7ee0f6c3706cc4b0cc0c2eed26e3192e0fe5b50b
owlbytes <neshah07@gmail.com>
2014-09-19 00:07:15 +0100

    delete seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,1 @@
 
-
-painting1 = Painting.create(image: "Abstract1.jpg")
-painting2 = Painting.create(image: "Abstract2.jpg")
-painting3 = Painting.create(image: "Abstract3.jpg")
-painting4 = Painting.create(image: "Abstract4.jpg")
-painting5 = Painting.create(image: "Abstract5.jpg")
-painting6 = Painting.create(image: "Abstract6.jpg")
-painting7 = Painting.create(image: "Abstract7.jpg")
-painting8 = Painting.create(image: "Abstract8.jpg")
-painting9 = Painting.create(image: "Abstract9.jpg")
-painting10 = Painting.create(image: "Abstract10.jpg")
-painting11 = Painting.create(image: "Abstract11.jpg")
-painting12 = Painting.create(image: "Abstract12.jpg")
-
