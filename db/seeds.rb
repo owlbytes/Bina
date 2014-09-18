db553aca40c52e03ebe009f850191c3042a13483
owlbytes <neshah07@gmail.com>
2014-09-18 23:42:00 +0100

    abstract seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+
+painting1 = Painting.create(image: "Abstract1.jpg")
+painting2 = Painting.create(image: "Abstract2.jpg")
+painting3 = Painting.create(image: "Abstract3.jpg")
+painting4 = Painting.create(image: "Abstract4.jpg")
+painting5 = Painting.create(image: "Abstract5.jpg")
+painting6 = Painting.create(image: "Abstract6.jpg")
+painting7 = Painting.create(image: "Abstract7.jpg")
+painting8 = Painting.create(image: "Abstract8.jpg")
+painting9 = Painting.create(image: "Abstract9.jpg")
+painting10 = Painting.create(image: "Abstract10.jpg")
+painting11 = Painting.create(image: "Abstract11.jpg")
+painting12 = Painting.create(image: "Abstract12.jpg")
+
+
+# attr_accessible :description, :title, :image
+# 'background.jpg'
+# user1 = User.create(first_name: "Nehita", last_name: "Shah", role: "admin", email: "neshah07@gmail.com", password: "password", confirmed_at: Time.now)
