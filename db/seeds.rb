52b485ca8116457056115d24b8e260fd7344d37e
owlbytes <neshah07@gmail.com>
2014-09-18 23:58:57 +0100

    .

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-
 
 
 painting1 = Painting.create(image: "Abstract1.jpg")
@@ -21,7 +13,3 @@ painting10 = Painting.create(image: "Abstract10.jpg")
 painting11 = Painting.create(image: "Abstract11.jpg")
 painting12 = Painting.create(image: "Abstract12.jpg")
 
-
-# attr_accessible :description, :title, :image
-# 'background.jpg'
-# user1 = User.create(first_name: "Nehita", last_name: "Shah", role: "admin", email: "neshah07@gmail.com", password: "password", confirmed_at: Time.now)
